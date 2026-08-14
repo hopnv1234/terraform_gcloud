df1de7a844e6e7cf216a1481cf26e22ce1c977a2
hop4393 <vhop93@gmail.com>
2026-08-14 10:35:49 +0700

    Update outputs.tf

--- my/modules/compute_instance/outputs.tf
+++ my/modules/compute_instance/outputs.tf
@@ -14,28 +14,77 @@
  * limitations under the License.
  */
 
-output "instances_self_links" {
-  description = "List of self-links for compute instances"
-  value       = google_compute_instance_from_template.compute_instance[*].self_link
+output "bastion_host_name" {
+  description = "Name of the bastion host VM"
+  value       = google_compute_instance.bastion-host.name
 }
 
-output "instances_details" {
-  description = "List of all details for compute instances"
-  sensitive   = true
-  value       = google_compute_instance_from_template.compute_instance[*]
+output "bastion_host_self_link" {
+  description = "Self link of the bastion host VM"
+  value       = google_compute_instance.bastion-host.self_link
 }
 
-output "available_zones" {
-  description = "List of available zones in region"
-  value       = data.google_compute_zones.available.names
+output "gitlab_ce_name" {
+  description = "Name of the GitLab CE VM"
+  value       = google_compute_instance.gitlab-ce.name
 }
 
-output "service_account_email" {
-  description = "The service account email associated with the instances."
-  value       = try(google_compute_instance_from_template.compute_instance[0].service_account[0].email, null)
+output "gitlab_ce_self_link" {
+  description = "Self link of the GitLab CE VM"
+  value       = google_compute_instance.gitlab-ce.self_link
 }
 
-output "instance_name" {
-  description = "The name of the first compute instance."
-  value       = try(google_compute_instance_from_template.compute_instance[0].name, "")
+output "tfe_name" {
+  description = "Name of the TFE VM"
+  value       = google_compute_instance.tfe.name
 }
+
+output "tfe_self_link" {
+  description = "Self link of the TFE VM"
+  value       = google_compute_instance.tfe.self_link
+}
+
+output "vault_server_name" {
+  description = "Name of the Vault server VM"
+  value       = google_compute_instance.vault-server.name
+}
+
+output "vault_server_self_link" {
+  description = "Self link of the Vault server VM"
+  value       = google_compute_instance.vault-server.self_link
+}
+
+output "vm_summary" {
+  description = "Summary map of all VM instances created by this module"
+  value = {
+    bastion_host = {
+      name    = google_compute_instance.bastion-host.name
+      zone    = google_compute_instance.bastion-host.zone
+      machine = google_compute_instance.bastion-host.machine_type
+      ip      = google_compute_instance.bastion-host.network_interface[0].network_ip
+      self_link = google_compute_instance.bastion-host.self_link
+    }
+    gitlab_ce = {
+      name    = google_compute_instance.gitlab-ce.name
+      zone    = google_compute_instance.gitlab-ce.zone
+      machine = google_compute_instance.gitlab-ce.machine_type
+      ip      = google_compute_instance.gitlab-ce.network_interface[0].network_ip
+      self_link = google_compute_instance.gitlab-ce.self_link
+    }
+    tfe = {
+      name    = google_compute_instance.tfe.name
+      zone    = google_compute_instance.tfe.zone
+      machine = google_compute_instance.tfe.machine_type
+      ip      = google_compute_instance.tfe.network_interface[0].network_ip
+      self_link = google_compute_instance.tfe.self_link
+    }
+    vault_server = {
+      name    = google_compute_instance.vault-server.name
+      zone    = google_compute_instance.vault-server.zone
+      machine = google_compute_instance.vault-server.machine_type
+      ip      = google_compute_instance.vault-server.network_interface[0].network_ip
+      self_link = google_compute_instance.vault-server.self_link
+    }
+  }
+}
+

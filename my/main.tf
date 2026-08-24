fcfe0d6328edafea2f2762a88eb4951a1d2fa712
hop4393 <vhop93@gmail.com>
2026-08-24 12:34:06 +0700

    Update main.tf

--- my/main.tf
+++ my/main.tf
@@ -55,8 +55,6 @@ module "compute_instance" {
   project_id       = var.project_id
   network          = module.vpc.network_name
   instance_template = ""
-
-  depends_on = [google_compute_router_nat.private_vms]
 }
 
 module "firewall_rules" {

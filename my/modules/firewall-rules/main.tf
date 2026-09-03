96d68ed39821c13859f8b15d4a1fe6c54e8620c7
hop4393 <vhop93@gmail.com>
2026-09-03 15:12:27 +0700

    Update main.tf

--- my/modules/firewall-rules/main.tf
+++ my/modules/firewall-rules/main.tf
@@ -54,3 +54,37 @@ resource "google_compute_firewall" "gitlab_web_ingress" {
 
   description = "Allow GitLab web access from the internet."
 }
+
+resource "google_compute_firewall" "bastion_internal_ingress" {
+  name    = "allow-internal-services-to-bastion"
+  project = var.project_id
+  network = var.network_name
+
+  direction     = "INGRESS"
+  priority      = 900
+  source_ranges = ["10.0.1.0/24", "10.0.4.0/24", "10.4.0.0/16", "10.5.0.0/20"]
+  target_tags   = ["bastion"]
+
+  allow {
+    protocol = "all"
+  }
+
+  description = "Allow all internal protocols and ports from Vault, Terraform, GKE, and GitLab networks to the bastion."
+}
+
+resource "google_compute_firewall" "bastion_internal_egress" {
+  name    = "allow-bastion-to-internal-services"
+  project = var.project_id
+  network = var.network_name
+
+  direction          = "EGRESS"
+  priority           = 900
+  destination_ranges = ["10.0.1.0/24", "10.0.4.0/24", "10.4.0.0/16", "10.5.0.0/20"]
+  target_tags        = ["bastion"]
+
+  allow {
+    protocol = "all"
+  }
+
+  description = "Allow all internal protocols and ports from the bastion to Vault, Terraform, GKE, and GitLab networks."
+}
